e23f34b5233bc3ca81eac188b97edea5cd6b88e4
Talita Oliveira <litaa.oliveira@gmail.com>
2019-08-26 15:44:15 -0300

    Refactor test: create a general describer for app.test

--- test/app.test.js
+++ test/app.test.js
@@ -4,32 +4,33 @@ chai.use(chaiHttp);
 const expect = require('chai').expect;
 const app = require('../app');
 
-describe('First Test', () => {
-    it('Should always pass', () => {
+describe('Hello World', () => {
+    it('First Test - Should always pass', () => {
         expect(true).to.be.true;
     });
-});
 
-describe('Hello World Route', (done) => {
-    it('Returns a 200 response', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((error, response) => {
-                if (error) done(error);
-                expect(response).to.have.status(200);
-                done();
-            });
-    });
-
-    it('Returns a "Hello World!" message', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((error, response) => {
-                if (error) done(error);
-                expect(response.body).to.be.deep.equal({
-                    message: 'Hello, world!'
+    describe('Hello World Route', (done) => {
+        it('Returns a 200 response', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((error, response) => {
+                    if (error) done(error);
+                    expect(response).to.have.status(200);
+                    done();
                 });
-                done();
-            });
+        });
+    
+        it('Returns a "Hello World!" message', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((error, response) => {
+                    if (error) done(error);
+                    expect(response.body).to.be.deep.equal({
+                        message: 'Hello, world!'
+                    });
+                    done();
+                });
+        });
     });
 });
+
