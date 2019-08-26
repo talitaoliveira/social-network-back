5ec0d3eb1cbd2d6ac66cb632da7a025ee339faee
Talita Oliveira <litaa.oliveira@gmail.com>
2019-08-26 16:58:46 -0300

    Refactoring response of route /users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const usersRoutes = () => {
     const router = express.Router();
     router.get('/', (req, res, next) => {
-        res.send('ok')
+        res.send({ message: 'Hello, Users!' })
     });
 
     return router;
